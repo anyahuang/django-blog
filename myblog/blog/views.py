51868ce2b995334886d8b331e1a5abcb7d36fcf9
anyahuang <huangpailu1981@gmail.com>
2021-10-20 10:35:42 +0200

    add blogs

--- myblog/blog/views.py
+++ myblog/blog/views.py
@@ -1,17 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
 from .models import Blog
 # Create your views here.
 def home(request):
     blog_posts = Blog.objects.all()
-    blog_list_html =""
-    for blog in blog_posts:
-        blog_list_html += f"<p><a href='blog/{blog.id}/'>{blog.title}<a></p>"
-    
-    html = f"<html><body>{blog_list_html}</body></html>" 
-    return HttpResponse(html)
-def blog_post(request, id=1):
+    context = {"blog_post":blog_posts}
+    return render(request,"blog/home.html",context)
+
+def blog_post(request, id):
     blog_post = Blog.objects.get(id=id)
-    html = f"<html><body><h1>{blog_post.title}</h1>{blog_post.date}<p>{blog_post.body}</p></body></html>"
-    return HttpResponse(html)
+    context = {"blog":blog }
+    return render(request, "blog/blog_post.html",context)
+    
