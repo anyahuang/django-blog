5f9b2bec2cb6e272222287051e98e76caac897a1
anyahuang <huangpailu1981@gmail.com>
2021-10-20 12:00:16 +0200

    fixed

--- myblog/blog/views.py
+++ myblog/blog/views.py
@@ -1,14 +1,13 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 from .models import Blog
-# Create your views here.
+
 def home(request):
     blog_posts = Blog.objects.all()
-    context = {"blog_post":blog_post}
+    context = {"blog_posts":blog_posts}
     return render(request,"blog/home.html",context)
 
-def blog_post(request, id):
+def blog_post(request, id): 
     blog_post = Blog.objects.get(id=id)
-    context = {"blog":blog }
+    context = {"blog": blog_post}
     return render(request, "blog/blog_post.html",context)
     
