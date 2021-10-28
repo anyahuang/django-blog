193915e6b16b779a72dde01c7357f9f9a1542218
anyahuang <huangpailu1981@gmail.com>
2021-10-28 16:08:29 +0200

    update

--- ablog/theblog/models.py
+++ ablog/theblog/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django.db.models.base import Model
 from django.urls import reverse
+
 class Post(models.Model):
   title = models.CharField(max_length=255)
   title_tag = models.CharField(max_length=255)
--- ablog/theblog/views.py
+++ ablog/theblog/views.py
@@ -1,7 +1,9 @@
 
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView,CreateView
+from django.views.generic import ListView, DetailView,CreateView, UpdateView
 from .models import Post
+from .forms import PostForm, EditForm
+
 
 # def home(request):
 #     return render(request, 'home.html',{})
@@ -15,6 +17,14 @@ class ArticleDetailView(DetailView):
   
 class AddPostView(CreateView): 
   model = Post
+  form_class = PostForm
   template_name = 'add_post.html'
   #fields = '__all__'
-  fields = ('title','body','author')
+  #fields = ('title','body','author')
+  
+class UpdatePostView(UpdateView):
+  model = Post
+  form_class = EditForm
+  template_name = 'update_post.html'
+  #fields = ['title','title_tag','body']
+    
